a3aa6cf2be28332cc62b664aab5702d7884bf5a4
Kenneth Gonzales <kenneth.gonzales@bicol-u.edu.ph>
2023-02-10 01:58:15 +0800

    UPDATE 3

--- script.js
+++ script.js
@@ -54,3 +54,67 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+
+$(document).ready(function() {
+    var items = $('.menu').children();
+    $(items[items.length - 1]).addClass('last-item');
+  });
+
+//filter array
+
+let filterarray = [];
+
+
+let galleryarray = [
+  {
+    id: 11,
+    name: "Americano Coffee",
+    scr : "Design Elements/Coffee Models/Basic/Americano Coffee.png",
+    cal : "10 cal",
+    mins : "5 mins",
+    price : "Php 120.00",
+
+  },
+  {
+    id: 12,
+    name: "Chocolate Coffee",
+    scr : "Design Elements/Coffee Models/Basic/Chocolate Coffee.png",
+    cal : "190 cal",
+    mins :"8 mins",
+    price : "Php 150.00",
+
+  },
+  {
+    id: 13,
+    name: "Espresso Coffee",
+    scr : "Design Elements/Coffee Models/Basic/Espresso Coffee.png",
+    cal : "20 cal",
+    mins : "5 mins",
+    price : "Php 190.00",
+  },
+  {
+    id: 14,
+    name: "Latte Coffee",
+    scr : "Design Elements/Coffee Models/Basic/Latte Coffee.png",
+    cal : "150 cal",
+    mins : "10 mins",
+    price : "Php 170.00",
+  },
+
+]
+
+
+
+function searchFunction() {
+  let input = document.getElementById('searchinput').value;
+  let items = document.getElementsByClassName('.product');
+
+  for (let i = 0; i < items.length; i++) {
+    if (!items[i].innerHTML.toLowerCase().includes(input.toLowerCase())) {
+      items[i].style.display = "none";
+    } else {
+      items[i].style.display = "block";
+    }
+  }
+}
