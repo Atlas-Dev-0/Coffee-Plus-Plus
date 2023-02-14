928c0f1a5015f3f15bf531086fe5bb545ab806a4
Kenneth Gonzales <kenneth.gonzales@bicol-u.edu.ph>
2023-02-14 16:16:08 +0800

    update

--- script.js
+++ script.js
@@ -68,3 +68,83 @@ function searchFunction() {
   }
 }
 
+
+
+function print_menulist() {
+  fetch('data/products.json')
+    .then(response => response.json())
+    .then(products => {
+      const productContainer = document.createElement("div");
+      productContainer.classList.add("menu", "container-fluid");
+      productContainer.id = "menu-list";
+
+      for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+
+        const productCard = document.createElement("div");
+        productCard.classList.add("product", "card");
+        productCard.id = product.id;
+
+        const image = document.createElement("div");
+        image.classList.add("image");
+
+        const productImage = document.createElement("img");
+        productImage.classList.add("product-image");
+        productImage.src = product.image;
+        productImage.alt = product.name;
+        image.appendChild(productImage);
+
+        const title = document.createElement("div");
+        title.classList.add("title");
+
+        const productName = document.createElement("h2");
+        productName.classList.add("product-name");
+        productName.textContent = product.name;
+        title.appendChild(productName);
+
+        const nutrifacts = document.createElement("div");
+        nutrifacts.classList.add("nutrifacts");
+
+        const calories = document.createElement("div");
+        calories.classList.add("calories");
+        calories.textContent = product.calories;
+        nutrifacts.appendChild(calories);
+        
+        const time = document.createElement("div");
+        time.classList.add("time");
+        time.textContent = product.time;
+        nutrifacts.appendChild(time);
+
+        const buyButton = document.createElement("a");
+        buyButton.classList.add("buybutton");
+        buyButton.href = "productinfo.html";
+
+        const productPrice = document.createElement("p");
+        productPrice.classList.add("product-price");
+        productPrice.textContent = product.price;
+        buyButton.appendChild(productPrice);
+
+        productCard.appendChild(image);
+        productCard.appendChild(title);
+        productCard.appendChild(nutrifacts);
+        productCard.appendChild(buyButton);
+        productContainer.appendChild(productCard);
+      }
+      document.body.appendChild(productContainer);
+      
+      
+      // CHANGE PRICE TO BUY
+      const productPrices = document.querySelectorAll(".product-price");
+      for (let i = 0; i < productPrices.length; i++) {
+        productPrices[i].addEventListener("mouseover", function ()
+         {
+          this.textContent = "Buy";
+        });
+        productPrices[i].addEventListener("mouseout", function () {
+          this.textContent = products[i].price;
+        });
+      }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
