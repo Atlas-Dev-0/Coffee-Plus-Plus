283529287101f94feb4a4e9b640f4365f6456766
Kenneth Gonzales <kenneth.gonzales@bicol-u.edu.ph>
2023-04-20 19:27:15 +0800

    removed not needed files and is working on add to cart module

---
+++ CodeStacks/PurchaseModule/functionScript.js
@@ -0,0 +1,9 @@
+/**
+ * This is a code that will be used for buy functionality for the user.
+ *
+ * 1.) The user will be picked what to buy
+ * 2.) The user will add the item to the cart
+ * 3.) The user will buy the item or cancel the purchase
+ * 4.) If the user buy the item then the user will be prompted with the purchase information
+ * 5.) And product will be delivered to the customer
+ */
