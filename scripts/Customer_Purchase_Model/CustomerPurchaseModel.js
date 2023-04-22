72dd465d1cc1309fdba2f27c403150daa90b5d7c
Kenneth Gonzales <kenneth.gonzales@bicol-u.edu.ph>
2023-04-22 15:30:26 +0800

    update 4.22

---
+++ scripts/Customer_Purchase_Model/CustomerPurchaseModel.js
@@ -0,0 +1,38 @@
+/**
+ * Customer Purchase Model
+ * --Model to store customer purchases
+ * Description: This model will be implemented to provide the customers the ability to add their products to the card list and purchase the products themselves.
+ *
+ */
+
+//cart is an array of product collection picked by the customer
+const cart = [];
+
+function generateOrderID() {
+  // Generate date string in format YYMMDD
+  const date = new Date();
+  const dateString = `${date.getFullYear().toString().slice(-2)}${(
+    date.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}${date.getDate().toString().padStart(2, "0")}`;
+
+  // Generate random 4-letter string
+  const randomString = Math.random().toString(36).substring(2, 6).toUpperCase();
+
+  // Get current number of orders placed on this date and increment
+  let orderNumber = localStorage.getItem(dateString);
+  if (!orderNumber) {
+    orderNumber = 1;
+  } else {
+    orderNumber = parseInt(orderNumber) + 1;
+  }
+  localStorage.setItem(dateString, orderNumber);
+
+  // Combine parts to create order ID
+  const orderID = `${dateString}-${randomString}-${orderNumber
+    .toString()
+    .padStart(3, "0")}`;
+
+  return orderID;
+}
